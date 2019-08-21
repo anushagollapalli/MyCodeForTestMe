71b368a6e9534f96fecac58ae0f8d8ab68c7e718
anushagollapalli <54269339+anushagollapalli@users.noreply.github.com>
2019-08-21 11:10:56 +0530

    Update Runnerclass.java

--- src/test/java/mystepdefinations/Runnerclass.java
+++ src/test/java/mystepdefinations/Runnerclass.java
@@ -6,7 +6,7 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features="C:\\Users\\training_h2a.06.15\\Desktop\\selenium workspace\\cucumber123\\myfeatures\\demoontags.feature",glue="mystepdefinations",tags= {"@Regression,@Smoke"})
+@CucumberOptions(features="C:\Users\training_h2a.06.15\Desktop\selenium workspace\cucumber123\myfeatures\demotourslogin.feature",glue="mystepdefinations",plugin= {"html:target/cucumber-htmlreport.html","json:target/cucumber.json"})
 
 public class Runnerclass {
 
